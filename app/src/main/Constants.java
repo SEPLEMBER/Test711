bc2985bb34ca69e448d871674cb492414caf3ede
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-12-27 12:22:01 +0700

    Add files via upload

---
+++ app/src/main/Constants.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of SimpleTextCrypt.
+ * Copyright (c) 2015-2020, Aidin Gharibnavaz <aidin@syndes.com>
+ *
+ * SimpleTextCrypt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleTextCrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimpleTextCrypt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syndes.javacomponents;
+
+/*
+ * Constants needed by the whole application.
+ */
+public class Constants {
+
+    public static final String PREFERENCES_KEY = "com.syndes.SimpleTextCrypt.Preferences";
+    public static final String ENCRYPTION_KEY_SETTINGS_KEY = "EncryptionKeySettingsKey";
+    public static final String PASSCODE_SETTINGS_KEY = "PasscodeSettingsKey";
+    public static final String LOCK_TIMEOUT_SETTINGS_KEY = "LockTimeoutSettingsKey";
+    public static final String DEFAULT_PASSCODE = "1111";
+}
