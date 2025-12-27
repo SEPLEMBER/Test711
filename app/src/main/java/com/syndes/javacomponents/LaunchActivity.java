5698b5cdd28c9b4990079d2715c8be45f0731cf6
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-12-27 13:59:52 +0700

    Create LaunchActivity.java

---
+++ app/src/main/java/com/syndes/javacomponents/LaunchActivity.java
@@ -0,0 +1,14 @@
+package com.syndes.javacomponents
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Устанавливаем layout с фоном и текстом
+        setContentView(R.layout.activity_launch);
+    }
+}
